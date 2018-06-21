476fdeaec2543890c54c9df1caeb69bb5fb188aa
Morris Jobke <hey@morrisjobke.de>
2018-06-21 11:41:47 +0200

    Render advisories in a long list
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- generator/template.php
+++ generator/template.php
@@ -5,10 +5,7 @@
 </div>
 </div>
 <div class="row">
-    <div class="col-md-4">
-        <?php get_template_part('advisories/advisory-side'); ?>
-    </div>
-    <div class="col-md-8">
+    <div class="col-md-12">
         <h2>~~TITLE~~ (~~IDENTIFIER~~)</h2>
         <p>~~DATE~~</p>
         <p>Risk level: <strong>~~LEVEL~~</strong></p>
